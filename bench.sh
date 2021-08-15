754d336298914de8d50fdb47b01465dd694bf2a1
starptech <deusdustin@gmail.com>
2021-08-15 13:22:50 +0200

    improve

--- bench.sh
+++ bench.sh
@@ -1,5 +1,5 @@
-echo -n "async-graphql-parser: " && cd rust-async-graphql-parser && cargo build --release -q && ./target/release/rust-async-graphql-parser $@
-echo -n "graphql-parser: " && cd .. && cd rust-graphql-parser && cargo build --release -q && ./target/release/rust-graphql-parser $@
+echo -n "async-graphql-parser: " && cd async-graphql-parser && cargo build --release -q && ./target/release/async-graphql-parser $@
+echo -n "graphql-parser: " && cd .. && cd graphql-parser && cargo build --release -q && ./target/release/graphql-parser $@
 echo -n "graphql-go-tools: " && cd .. && cd graphql-go-tools && go build -ldflags "-s -w" -o test && ./test $@
 echo -n "go-graphql: " && cd .. && cd go-graphql && go build -ldflags "-s -w" -o test && ./test $@
 echo -n "gqlparser: " && cd .. && cd gqlparser && go build -ldflags "-s -w" -o test && ./test $@

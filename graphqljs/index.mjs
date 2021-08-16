4d8115d8f81288fe8cae468aea81cb22c18874dc
Jonah Snider <jonah@jonahsnider.com>
2021-08-16 16:15:13 -0700

    refactor: modernize benchmark implementation

--- graphqljs/index.mjs
+++ graphqljs/index.mjs
@@ -1,23 +1,24 @@
-import { buildSchema, parse } from 'graphql'
 import { readFile } from 'fs/promises'
+import { parse } from 'graphql'
+import { performance } from 'perf_hooks'
 
 const schema = await readFile(process.argv[2], 'utf-8')
 
 // warmup (jit)
 
 for (let index = 0; index < 10; index++) {
-    const hrstart = process.hrtime()
+  const start = performance.now()
     parse(schema)
     if (index === 0) {
-        const hrend = process.hrtime(hrstart)
-        console.info('graphqljs (cold): %dms', hrend[1] / 1000000)
+        const end = performance.now()
+        console.info(`graphqljs (cold): ${end - start}ms`)
     }
 
 }
 
-const hrstart = process.hrtime()
+const start = performance.now()
 
 parse(schema)
 
-const hrend = process.hrtime(hrstart)
-console.info('graphqljs (warmed): %dms', hrend[1] / 1000000)
+const end = performance.now()
+console.info(`graphqljs (warmed): ${end - start}ms`, )

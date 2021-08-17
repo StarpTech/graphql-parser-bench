36a7c1fc54dd39c9a41a63fadd3263c90810b92b
Jonah Snider <jonah@jonahsnider.com>
2021-08-16 18:22:06 -0700

    style: fix formatting

--- graphqljs/index.mjs
+++ graphqljs/index.mjs
@@ -7,13 +7,12 @@ const schema = await readFile(process.argv[2], 'utf-8')
 // warmup (jit)
 
 for (let index = 0; index < 10; index++) {
-  const start = performance.now()
+    const start = performance.now()
     parse(schema)
     if (index === 0) {
         const end = performance.now()
         console.info(`graphqljs (cold): ${end - start}ms`)
     }
-
 }
 
 const start = performance.now()
